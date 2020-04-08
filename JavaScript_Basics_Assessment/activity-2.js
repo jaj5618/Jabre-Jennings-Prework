9bf06b2c6da321252ef44a6d0620b964f45503d2
Jabre Jennings <jaj5618@gmail.com>
2020-04-08 11:28:56 -0400

    Initial commit

---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,9 @@
+let name = prompt('What is your name?');
+
+    if ( name > 'glee'){
+     alert('Your name is greater than four characters');
+    } 
+    else {
+     alert('Your name is less than four characters');
+    } 
+    
---
+++ JavaScript_Basics_Assessment/activity-3.js
@@ -0,0 +1,13 @@
+var students = ['Mystee', 'Madison', 'Jaxon'];
+
+var i;
+    for(i = 1; i<= 3; i++){
+        var studentname =prompt('Enter student' + i + 'name');
+
+        students.push(studentname);
+    }
+
+    for(i = 0; i < student.length; i++){
+        console.log(students[i] + '')
+    }
+
